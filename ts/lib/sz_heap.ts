291247549aca1761a03e22067848faac67b268b0
primaryshazi <primaryemail@qq.com>
2024-01-08 12:10:41 +0800

    更新堆

--- ts/lib/sz_heap.ts
+++ ts/lib/sz_heap.ts
@@ -1,12 +1,14 @@
 import { szcommon } from "./sz_common";
 import { szdefine } from "./sz_define";
 
+export type szheap_value_type<T> = T | undefined;
+
 export class SZHeap<T> {
     // 比较函数，用于构建最大堆或者最小堆
-    private compare_: szcommon.conditonCompare<T | undefined> = szcommon.greaterCompare;
+    private compare_: szcommon.conditonCompare<szheap_value_type<T>> = szcommon.greaterCompare;
 
     // 储存元素
-    private data_: Array<T | undefined> = [];
+    private data_: Array<szheap_value_type<T>> = [];
 
     // 元素个数
     private size_: number = 0;
@@ -15,9 +17,9 @@ export class SZHeap<T> {
      * 构建堆
      * @param compareFunc 默认最大堆
      */
-    public constructor(compareFunc: szcommon.conditonCompare<T | undefined> = szcommon.greaterCompare) {
+    public constructor(compareFunc: szcommon.conditonCompare<szheap_value_type<T>> = szcommon.greaterCompare) {
         this.compare_ = compareFunc;
-        this.data_ = new Array<T | undefined>(szdefine.DEFAULT_CAPACITY).fill(undefined);
+        this.data_ = new Array<szheap_value_type<T>>(szdefine.DEFAULT_CAPACITY).fill(undefined);
         this.size_ = 0;
     }
 
@@ -70,7 +72,7 @@ export class SZHeap<T> {
         if (c < this.data_.length) {
             this.data_.length = c;
         } else {
-            this.data_.push(...new Array<T | undefined>(c - this.data_.length).fill(undefined));
+            this.data_.push(...new Array<szheap_value_type<T>>(c - this.data_.length).fill(undefined));
         }
 
         if (this.size_ > this.data_.length) {
@@ -79,10 +81,17 @@ export class SZHeap<T> {
     }
 
     /**
+     * 获取比较函数
+     */
+    public get compare(): szcommon.conditonCompare<szheap_value_type<T>> {
+        return this.compare_;
+    }
+
+    /**
      * 获取栈顶元素
      * @returns 
      */
-    public top(): T | undefined {
+    public top(): szheap_value_type<T> {
         if (this.size_ > 0 && this.data_.length > 0) {
             return this.data_[0];
         }
@@ -100,18 +109,20 @@ export class SZHeap<T> {
         }
 
         if (this.size >= this.data_.length) {
-            this.resize();
+            this.expandCapacity();
         }
 
-        let cur = this.size_;
-        let next = Math.floor(cur / 2);
-        while (cur > 0 && this.compare_(this.data_[next], value) == 1) {
-            this.data_[cur] = this.data_[next];
-            cur = next;
-            next = Math.floor(cur / 2);
-        }
-        this.data_[cur] = value;
+        this.data_[this.size_] = value;
         this.size_++;
+
+        let cur = this.size_ - 1;
+        let next = Math.floor((cur - 1) / 2);
+        while (cur > 0 && this.compare_(this.data_[cur], this.data_[next]) == -1) {
+            [this.data_[cur], this.data_[next]] = [this.data_[next], this.data_[cur]];
+
+            cur = next;
+            next = Math.floor((cur - 1) / 2);
+        }
     }
 
     /**
@@ -122,32 +133,78 @@ export class SZHeap<T> {
             return;
         }
 
-        let final = this.size_ - 1;
-        let last = this.data_[final];
-        let cur = 0;
-        let child = 0;
+        this.data_[0] = this.data_[this.size_ - 1];
+        this.size_--;
 
-        this.data_[final] = undefined;
-        for (cur = 0; cur * 2 < final; cur = child) {
-            child = cur * 2 + 1;
-            if (child + 1 < final && this.compare_(this.data_[child], this.data_[child + 1]) == 1) {
-                child++;
+        let cur = 0;
+        while (cur < this.size_) {
+            let next = cur;
+            let left = 2 * cur + 1;
+            let right = 2 * cur + 2;
+
+            if (left < this.size_ && this.compare_(this.data_[left], this.data_[next]) == -1) {
+                next = left;
             }
 
-            if (this.compare_(last, this.data_[child]) == -1) {
-                break;
+            if (right < this.size_ && this.compare_(this.data_[right], this.data_[next]) == -1) {
+                next = right;
+            }
+
+            if (next != cur) {
+                [this.data_[cur], this.data_[next]] = [this.data_[next], this.data_[cur]];
+                cur = next;
             } else {
-                this.data_[cur] = this.data_[child];
+                break;
             }
         }
-        this.data_[cur] = last;
-        this.size_ = final;
+
+        this.data_[this.size_] = undefined;
+    }
+
+    /**
+     * 收缩
+     */
+    public shrink() {
+        if (this.data_.length > this.size_) {
+            this.data_.length = Math.max(this.size_, 1);
+        }
     }
 
     /**
      * 重新扩容
      */
-    private resize() {
-        this.data_.push(...new Array<T | undefined>(Math.ceil(this.data_.length * (szdefine.DEFAULT_GROWTH_FACTOR - 1))).fill(undefined));
+    private expandCapacity() {
+        this.data_.push(...new Array<szheap_value_type<T>>(Math.ceil(this.data_.length * (szdefine.DEFAULT_GROWTH_FACTOR - 1))).fill(undefined));
+    }
+
+    /**
+     * 构建一个堆
+     * @param array 
+     * @param compare 
+     */
+    static buildHeap<T>(array: Array<szheap_value_type<T>>, compare: szcommon.conditonCompare<szheap_value_type<T>> = szcommon.greaterCompare) {
+        for (let index = array.length - 1; index >= 0; index--) {
+            let cur = index;
+            while (cur < array.length) {
+                let next = cur;
+                let left = 2 * cur + 1;
+                let right = 2 * cur + 2;
+
+                if (left < array.length && compare(array[left], array[next]) == -1) {
+                    next = left;
+                }
+
+                if (right < array.length && compare(array[right], array[next]) == -1) {
+                    next = right;
+                }
+
+                if (next != cur) {
+                    [array[cur], array[next]] = [array[next], array[cur]];
+                    cur = next;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
