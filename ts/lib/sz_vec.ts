4d63cb32235e3e68ed0d6b11a137b30079841129
primaryshazi <primaryemail@qq.com>
2024-02-21 16:28:13 +0800

    修改ts vec

--- ts/lib/sz_vec.ts
+++ ts/lib/sz_vec.ts
@@ -2,29 +2,16 @@
  * 2维向量
  */
 export class SZVec2 {
-    protected x_: number = 0;
-    public get x(): number {
-        return this.x_;
-    }
-    public set x(x: number) {
-        this.x_ = x;
-    }
-
-    protected y_: number = 0;
-    public get y(): number {
-        return this.y_;
-    }
-    public set y(y: number) {
-        this.y_ = y;
-    }
+    public x: number = 0;
+    public y: number = 0;
 
     constructor(x?: number, y?: number) {
         if (x) {
-            this.x_ = x;
+            this.x = x;
         }
 
         if (y) {
-            this.y_ = y;
+            this.y = y;
         }
     }
 
@@ -240,12 +227,32 @@ export class SZVec2 {
     }
 
     /**
+     * 向量点乘
+     * @param x
+     * @param y
+     * @returns
+     */
+    public dot2f(x: number, y: number): number {
+        return this.x * x + this.y * y;
+    }
+
+    /**
      * 向量叉乘
      * @param vec
      * @returns
      */
     public cross(vec: SZVec2): number {
         return this.x * vec.y - this.y * vec.x;
+    }
+
+    /**
+     * 向量叉乘
+     * @param x
+     * @param y
+     * @returns
+     */
+    public cross2f(x: number, y: number): number {
+        return this.x * y - this.y * x;
     }
 
     /**
@@ -256,6 +263,17 @@ export class SZVec2 {
     public project(vec: SZVec2): SZVec2 {
         const rate = this.dot(vec) / vec.length();
         return new SZVec2(vec.x * rate, vec.y * rate);
+    }
+
+    /**
+     * 在指定向量上的投影
+     * @param x
+     * @param y
+     * @returns
+     */
+    public project2f(x: number, y: number): SZVec2 {
+        const rate = this.dot2f(x, y) / this.length();
+        return new SZVec2(this.x * rate, this.y * rate);
     }
 
     /**
@@ -297,6 +315,254 @@ export class SZVec2 {
         const y = this.x * sinRad + this.y * cosRad;
         this.x = x;
         this.y = y;
+        return this;
+    }
+}
+
+/**
+ * 3维向量
+ */
+export class SZVec3 {
+    public x: number = 0;
+    public y: number = 0;
+    public z: number = 0;
+
+    constructor(x?: number, y?: number, z?: number) {
+        if (x) {
+            this.x = x;
+        }
+
+        if (y) {
+            this.y = y;
+        }
+
+        if (z) {
+            this.z = z;
+        }
+    }
+
+    // 零向量
+    public static readonly ZERO = new SZVec3(0, 0, 0);
+
+    // x正半轴
+    public static readonly UNIT_X = new SZVec3(1, 0, 0);
+
+    // x负半轴
+    public static readonly UNIT_NEG_X = new SZVec3(-1, 0, 0);
+
+    // y正半轴
+    public static readonly UNIT_Y = new SZVec3(0, 1, 0);
+
+    // y负半轴
+    public static readonly UNIT_NEG_Y = new SZVec3(0, -1, 0);
+
+    // z正半轴
+    public static readonly UNIT_Z = new SZVec3(0, 0, 1);
+
+    // z负半轴
+    public static readonly UNIT_NEG_Z = new SZVec3(0, 0, -1);
+
+    // 单位向量
+    public static readonly ONE = new SZVec3(1, 1, 0);
+
+    // 单位负向量
+    public static readonly NEG_ONE = new SZVec3(-1, -1, -1);
+
+    /**
+     * 克隆
+     * @returns
+     */
+    public clone(): SZVec3 {
+        return new SZVec3(this.x, this.y, this.z);
+    }
+
+    /**
+     * 向量长度
+     * @returns
+     */
+    public length(): number {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    /**
+     * 加
+     * @param vec
+     * @returns
+     */
+    public add(vec: SZVec3): SZVec3 {
+        this.x += vec.x;
+        this.y += vec.y;
+        this.z += vec.z;
+        return this;
+    }
+
+    /**
+     * 加
+     * @param x
+     * @param y
+     * @param z
+     * @returns
+     */
+    public add3f(x: number, y: number, z: number): SZVec3 {
+        this.x += x;
+        this.y += y;
+        this.z += z;
+        return this;
+    }
+
+    /**
+     * 减
+     * @param vec
+     * @returns
+     */
+    public sub(vec: SZVec3): SZVec3 {
+        this.x -= vec.x;
+        this.y -= vec.y;
+        this.z -= vec.z;
+        return this;
+    }
+
+    /**
+     * 减法
+     * @param x
+     * @param y
+     * @param z
+     * @returns
+     */
+    public sub2f(x: number, y: number, z: number): SZVec3 {
+        this.x -= x;
+        this.y -= y;
+        this.z -= z;
+        return this;
+    }
+
+    /**
+     * 乘
+     * @param vec
+     * @returns
+     */
+    public mul(vec: SZVec3): SZVec3 {
+        this.x *= vec.x;
+        this.y *= vec.y;
+        this.z *= vec.z;
+        return this;
+    }
+
+    /**
+     * 乘
+     * @param x
+     * @param y
+     * @param z
+     * @returns
+     */
+    public mul3f(x: number, y: number, z: number): SZVec3 {
+        this.x *= x;
+        this.y *= y;
+        this.z *= z;
+        return this;
+    }
+
+    /**
+     * 除
+     * @param vec
+     * @returns
+     */
+    public div(vec: SZVec3): SZVec3 {
+        this.x /= vec.x;
+        this.y /= vec.y;
+        this.z / vec.z;
+        return this;
+    }
+
+    /**
+     * 除
+     * @param x
+     * @param y
+     * @param z
+     * @returns
+     */
+    public div3f(x: number, y: number, z: number): SZVec3 {
+        this.x /= x;
+        this.y /= y;
+        this.z /= z;
+        return this;
+    }
+
+    /**
+     * 判断是否相等
+     * @param vec
+     * @param epsilon
+     * @returns
+     */
+    public equals(vec: SZVec3, epsilon?: number): boolean {
+        if (epsilon != undefined) {
+            return Math.abs(this.x - vec.x) < epsilon && Math.abs(this.y - vec.y) < epsilon && Math.abs(this.z - vec.z) < epsilon;
+        }
+        return this.x == vec.x && this.y == vec.y && this.z == vec.z;
+    }
+
+    /**
+     * 判断是否相等
+     * @param x
+     * @param y
+     * @param z
+     * @param epsilon
+     * @returns
+     */
+    public equals3f(x: number, y: number, z: number, epsilon?: number): boolean {
+        if (epsilon != undefined) {
+            return Math.abs(this.x - x) < epsilon && Math.abs(this.y - y) < epsilon && Math.abs(this.z - z) < epsilon;
+        }
+        return this.x == x && this.y == y && this.z == z;
+    }
+
+    /**
+     * 单位化向量
+     * @returns
+     */
+    public normalize(): SZVec3 {
+        const len = this.length();
+        if (len > 0) {
+            this.x /= len;
+            this.y /= len;
+            this.z /= len;
+        }
+        return this;
+    }
+
+    /**
+     * 缩放向量
+     * @param scale
+     * @returns
+     */
+    public scaler(scale: number) {
+        this.x *= scale;
+        this.y *= scale;
+        this.z *= scale;
+        return this;
+    }
+
+    /**
+     * 向量取反
+     * @returns
+     */
+    public negative(): SZVec3 {
+        this.x = -this.x;
+        this.y = -this.y;
+        this.y = -this.y;
+        return this;
+    }
+
+    /**
+     * 线性向量插值
+     * @param vec
+     * @param t
+     * @returns
+     */
+    public lerp(vec: SZVec3, t: number): SZVec3 {
+        this.x += (vec.x - this.x) * t;
+        this.y += (vec.y - this.y) * t;
+        this.z += (vec.z - this.z) * t;
         return this;
     }
 }
